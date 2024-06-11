97c3faca196e68be8810ebe78485dc540e8a9844
pbernal0990 <pbernal@cibernetica.net>
2024-06-11 17:21:04 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,4 +46,6 @@ if time_to_insert:
 
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.json())
+#st.text(fruityvice_response.json())
+
+fv_df=st.dataframe(data=fruityvice_response.json(),use_container_width=True)
